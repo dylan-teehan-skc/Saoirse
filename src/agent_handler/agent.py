9c4d2f39ad10df16300e5f57de64dbd58a0a634b
Jack Casey <jackcasey614@gmail.com>
2024-08-27 15:06:07 +0100

    add output_json formatting and update prompt.txt and response writing

--- src/agent_handler/agent.py
+++ src/agent_handler/agent.py
@@ -1,14 +1,17 @@
+import os
+from textwrap import dedent
 from llm_wrap_lib.llm_wrap import DynamicLLMWrapper
 from agent_handler.task import Task
-import os
 from tool_handler.tool import Tool
 
+
 class Agent:
-    def __init__(self, name, goal, backstory, verbose):
+    def __init__(self, name, goal, backstory, verbose, json_output=False):
         self._name = name
         self._goal = goal
         self._backstory = backstory
         self._verbose = verbose
+        self._json_output = json_output
         self._current_task = None
         self._llm_wrapper = DynamicLLMWrapper()  # Initialize once
         self.tools = []
@@ -46,6 +49,12 @@ class Agent:
     def set_verbose(self, new_verbose):
         self._verbose = new_verbose
         
+    def get_json_output(self):
+        return self._json_output
+
+    def set_json_output(self, json_output):
+        self._json_output = json_output
+
     def execute_task(self):
         if self._current_task is None:
             raise ValueError("No task is set for the agent")
@@ -66,17 +75,32 @@ class Agent:
             agent_goal=self.get_goal(),
             agent_backstory=self.get_backstory()
         )
+
+        if self._json_output:
+            formatted_prompt += "\nPlease provide your response in JSON format."
         
         response = self._llm_wrapper.call_model(formatted_prompt, tools=tools)
 
-        with open('response.txt', 'a', encoding='utf-8') as file:
-            file.write(f"{self.get_name()} response: ")
-            file.write(f"{response.get_response_content()} \n")
-            file.write(f"Cost for response: {str(response.get_cost())}")
-            file.write("\n\n\n\n")
+        self.write_response_to_file(description, expected_output, response)
 
         return response.get_response_content()
 
 def read_prompt_from_file(file_path):
     with open(file_path, 'r') as file:
         return file.read().strip()
+    
+
+def write_response_to_file(self, description, expected_output, response):
+        response_content = dedent(f"""
+        <user>
+            Agent: {self.get_name()}
+            description of task: {description}
+            Expected Output: {expected_output}
+        </user>
+        <ai_response>
+            Response: {response.get_response_content()}
+            Cost for response: {response.get_cost()}
+        </ai_response>
+        """)
+        with open('response.txt', 'a', encoding='utf-8') as file:
+            file.write(response_content)
